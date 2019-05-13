6d03533ee53efb3bbd418b0917b4565064a8fe2b
mrstexx <stefi5miljevic@gmail.com>
2019-05-13 21:44:15 +0200

    Design GUI - first step

--- src/com/sp/application/Main.java
+++ src/com/sp/application/Main.java
@@ -1,27 +1,30 @@
 package com.sp.application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Window.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    private static final int DEFAULT_WIDTH = 640;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            VBox root = (VBox) FXMLLoader.load(getClass().getResource("Window.fxml"));
+            Scene scene = new Scene(root, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("FHT - Shortest Path Application");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/com/sp/application/WindowController.java
+++ src/com/sp/application/WindowController.java
@@ -1,5 +1,86 @@
 package com.sp.application;
 
+import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
 public class WindowController {
-	
+
+    private String fileName = "";
+    @FXML
+    private MenuItem menuImportNewData;
+    @FXML
+    private MenuItem menuCloseWindow;
+
+    @FXML
+    private MenuItem menuAboutUs;
+
+    @FXML
+    private Label labelFileName;
+
+    @FXML
+    private TextField fieldStartStation;
+
+    @FXML
+    private TextField fieldEndStation;
+
+    @FXML
+    private Button btnGetPath;
+
+    @FXML
+    private AnchorPane resultArea;
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void importNewData(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("Text Files", "*.txt"));
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+        this.fileName = selectedFile.getAbsolutePath();
+        labelFileName.setText("Imported from: " + selectedFile.getAbsolutePath());
+    }
+
+    @FXML
+    void menuAbout(ActionEvent event) {
+        String title = "About us";
+        String message = "FH Technikum Wien 2019\nFinal Project Algorithms and Data Structures\n\nAuthors:"
+                + "\nStefan Miljevic - BIF2C\nDaniel Krottendorfer - BIF2C";
+        showAlertMessage(message, title, AlertType.INFORMATION);
+    }
+
+    @FXML
+    void execGetPath(ActionEvent event) {
+        if (this.fieldStartStation.getText() == "" || this.fieldEndStation.getText() == "" || this.fileName == "") {
+            String title = "Missing data";
+            String message = "Something went wrong. Check you entered all fields.";
+            showAlertMessage(message, title, AlertType.ERROR);
+            return;
+        }
+        
+    }
+
+    private void showAlertMessage(String message, String title, AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 }
